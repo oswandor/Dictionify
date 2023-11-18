bcb970276ca0d616ea10ec1417934a44d4faea06
oswandor <oswandor26@gmail.com>
2023-11-18 06:17:13 -0600

    se agrego boton eliminar y su respectivo metodo

--- src/views/FavoritosPage.vue
+++ src/views/FavoritosPage.vue
@@ -1,38 +1,57 @@
 <template>
     <ion-page>
         <ion-content>
+            <h1>Lista de Favoritos</h1>
 
-            <div>
-                <h1>Lista de Favoritos</h1>
-                <ul>
-                    <li v-for="favorite in favoritesList" :key="favorite._id.$oid">
-                        <h2>{{ favorite.favoritos.word }}</h2>
-                        <p><strong>Definición:</strong> {{ favorite.favoritos.definition }}</p>
-                        <p><strong>Ejemplos:</strong></p>
-                        <ul>
-                            <li v-for="example in favorite.favoritos.examples" :key="example">{{ example }}</li>
-                        </ul>
-                        <p><strong>Sinónimos:</strong> {{ favorite.favoritos.synonyms.join(', ') }}</p>
-                        <p><strong>Antónimos:</strong> {{ favorite.favoritos.antonyms.join(', ') }}</p>
-                    </li>
-                </ul>
-            </div>
+            <ion-list>
+                <ion-item v-for="favorite in favoritesList" :key="favorite._id.$oid">
+                    <ion-card>
+                        <ion-card-header>
+                            <ion-card-subtitle>{{ favorite.favoritos.word }}</ion-card-subtitle>
+                            <ion-card-title>{{ favorite.favoritos.definition }}</ion-card-title>
+                        </ion-card-header>
+
+                        <ion-card-content>
+                            <ion-label><strong>Ejemplos:</strong></ion-label>
+                            <ion-list>
+                                <ion-item v-for="example in favorite.favoritos.examples" :key="example">
+                                    {{ example }}
+                                </ion-item>
+                            </ion-list>
+
+                            <ion-label><strong>Sinónimos:</strong> {{ favorite.favoritos.synonyms.join(', ') }}</ion-label>
+                            <ion-label><strong>Antónimos:</strong> {{ favorite.favoritos.antonyms.join(', ') }}</ion-label>
+                            <!-- Botón de eliminación -->
+                            <ion-button @click="deleteFavorite(favorite._id.$oid)">
+                                <ion-icon :icon="trash"></ion-icon>
+                            </ion-button>
+
+
+                        </ion-card-content>
+                    </ion-card>
+                </ion-item>
+            </ion-list>
         </ion-content>
     </ion-page>
 </template>
 <script>
-import { IonCol, alertController, IonIcon, IonGrid, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+import { IonList, IonCol, alertController, IonIcon, IonGrid, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 import { Storage } from '@ionic/storage';
 import axios from 'axios';
+import { trash } from 'ionicons/icons';
+
+
 export default {
     name: 'FavoritosPage',
     components: {
 
-        IonCol, alertController, IonIcon, IonGrid, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle
+        IonList, IonCol, alertController, IonIcon, IonGrid, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle
     },
     data() {
         return {
             favoritesList: [],
+            trashIcon: trash,
+            uid: '' 
         };
     },
 
@@ -44,16 +63,38 @@ export default {
                 const response = await axios.get(`${apiUrl}/allUserFavorites/${uid}`);
                 this.favoritesList = response.data;
 
-                console.log(this.favoritesList); 
+                console.log(this.favoritesList);
             } catch (error) {
                 console.error('Error al obtener favoritos del usuario:', error);
             }
         },
+
+        // Función para eliminar un favorito
+        async deleteFavorite(favoriteId) {
+            try {
+
+                console.log(favoriteId)
+               
+                const apiUrl = 'https://conteinaerappsdiccionary.calmmoss-65dacf7d.eastus.azurecontainerapps.io';
+                await axios.delete(`${apiUrl}/deleteUserFavoritesByIdDiccionary/${this.uid}/${favoriteId}`);
+                
+
+                const url =  `${apiUrl}/deleteUserFavoritesByIdDiccionary/${this.uid}/${favoriteId}` ; 
+                console.log(url) 
+                console.log(this.uid)
+                // Vuelve a cargar la lista después de eliminar
+                this.getAllUserFavorites(this.uid);
+            } catch (error) {
+                console.error('Error al eliminar el favorito:', error);
+            }
+        },
+
+
     },
 
 
     // para 
-   async mounted() {
+    async mounted() {
 
         // Instanciar el servicio Storage
         let storage = new Storage();
@@ -62,12 +103,12 @@ export default {
         await storage.create();
 
         // Obtener datos del almacenamiento
-        const uid = await storage.get('uid');
+        this.uid = await storage.get('uid');
 
         // Hacer algo con los datos recuperados
-        console.log('UID from Storage:', uid);
+        console.log('UID from Storage:', this.uid);
 
-        this.getAllUserFavorites(uid);
+        this.getAllUserFavorites(this.uid);
 
     },
 }
